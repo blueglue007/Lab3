8b4733fc961e2698709f66006b702e71133f18de
blueglue007 <blueglue007@hotmail.com>
2021-03-07 21:38:55 -0700

    V1.1

--- MatrixThread.java
+++ MatrixThread.java
@@ -1,11 +1,8 @@
-
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class MatrixThread {
-
-
     public static void main(String[] args) {
         if (args.length == 0) {
             System.out.println("Error. Please Enter correct path.");
@@ -45,83 +42,72 @@ public class MatrixThread {
         }
 
 
-        int miniMetrixRowLenght = Row/2;
-        int miniMetrixColLenght = Col/2;
+        int miniMatrixRowLength = Row/2;
+        int miniMatrixColLength = Col/2;
         if (Row % 2 != 0) {
-            miniMetrixRowLenght ++;
+            miniMatrixRowLength ++;
         }
         if (Col % 2 != 0) {
-            miniMetrixColLenght ++;
+            miniMatrixColLength ++;
         }
-        int[][] ANW = new int[miniMetrixRowLenght][miniMetrixColLenght];
-        int[][] ANE = new int[miniMetrixRowLenght][miniMetrixColLenght];
-        int[][] ASW = new int[miniMetrixRowLenght][miniMetrixColLenght];
-        int[][] ASE = new int[miniMetrixRowLenght][miniMetrixColLenght];
-        int[][] BNW = new int[miniMetrixRowLenght][miniMetrixColLenght];
-        int[][] BNE = new int[miniMetrixRowLenght][miniMetrixColLenght];
-        int[][] BSW = new int[miniMetrixRowLenght][miniMetrixColLenght];
-        int[][] BSE = new int[miniMetrixRowLenght][miniMetrixColLenght];
+        int[][] A00 = new int[miniMatrixRowLength][miniMatrixColLength];
+        int[][] A01 = new int[miniMatrixRowLength][miniMatrixColLength];
+        int[][] A10 = new int[miniMatrixRowLength][miniMatrixColLength];
+        int[][] A11 = new int[miniMatrixRowLength][miniMatrixColLength];
+        int[][] B00 = new int[miniMatrixRowLength][miniMatrixColLength];
+        int[][] B01 = new int[miniMatrixRowLength][miniMatrixColLength];
+        int[][] B10 = new int[miniMatrixRowLength][miniMatrixColLength];
+        int[][] B11 = new int[miniMatrixRowLength][miniMatrixColLength];
 
-        for (int r = 0; r < miniMetrixRowLenght; r++) {
-            for (int c = 0; c < miniMetrixColLenght; c++) {
-                ANW[r][c] = A[r][c];
-                ANE[r][c] = A[r][c+miniMetrixColLenght];
-                ASW[r][c] = A[r+miniMetrixRowLenght][c];
-                ASE[r][c] = A[r+miniMetrixRowLenght][c+miniMetrixColLenght];
-                BNW[r][c] = B[r][c];
-                BNE[r][c] = B[r][c+miniMetrixColLenght];
-                BSW[r][c] = B[r+miniMetrixRowLenght][c];
-                BSE[r][c] = B[r+miniMetrixRowLenght][c+miniMetrixColLenght];
+        for (int r = 0; r < miniMatrixRowLength; r++) {
+            for (int c = 0; c < miniMatrixColLength; c++) {
+                A00[r][c] = A[r][c];
+                A01[r][c] = A[r][c+miniMatrixColLength];
+                A10[r][c] = A[r+miniMatrixRowLength][c];
+                A11[r][c] = A[r+miniMatrixRowLength][c+miniMatrixColLength];
+                B00[r][c] = B[r][c];
+                B01[r][c] = B[r][c+miniMatrixColLength];
+                B10[r][c] = B[r+miniMatrixRowLength][c];
+                B11[r][c] = B[r+miniMatrixRowLength][c+miniMatrixColLength];
             }
         }
 
-
-
-
-
-
-
-        addMatrix NE = new addMatrix(ANE, BNE);
-        addMatrix NW = new addMatrix(ANW, BNW);
-        addMatrix SE = new addMatrix(ASE, BSE);
-        addMatrix SW = new addMatrix(ASW, BSW);
-        NE.start();
+        addMatrix NW = new addMatrix(A00, B00);
+        addMatrix NE = new addMatrix(A01, B01);
+        addMatrix SW = new addMatrix(A10, B10);
+        addMatrix SE = new addMatrix(A11, B11);
         NW.start();
-        SE.start();
+        NE.start();
         SW.start();
+        SE.start();
 
-        int[][] CNW = NW.getResult();
-        int[][] CNE = NE.getResult();
-        int[][] CSW = SW.getResult();
-        int[][] CSE = SE.getResult();
+        try{
+            NW.join();
+            NE.join();
+            SW.join();
+            SE.join();
+        }catch(InterruptedException e){
+            System.out.println("Interrupted");
+        }
+
+        int[][] C00 = NW.getResult();
+        int[][] C01 = NE.getResult();
+        int[][] C10 = SW.getResult();
+        int[][] C11 = SE.getResult();
 
 
-
-        for (int r = 0; r < miniMetrixRowLenght; r++) {
-            for (int c = 0; c < miniMetrixColLenght; c++) {
-                C[r][c] = CNW[r][c];
-                C[r][c+miniMetrixColLenght] = CNE[r][c];
-                C[r+miniMetrixRowLenght][c] = CSW[r][c];
-                C[r+miniMetrixRowLenght][c+miniMetrixColLenght] = CSE[r][c];
+        for (int r = 0; r < miniMatrixRowLength; r++) {
+            for (int c = 0; c < miniMatrixColLength; c++) {
+                C[r][c] = C00[r][c];
+                C[r][c+miniMatrixColLength] = C01[r][c];
+                C[r+miniMatrixRowLength][c] = C10[r][c];
+                C[r+miniMatrixRowLength][c+miniMatrixColLength] = C11[r][c];
             }
         }
 
-        printMatrix(CNW);
-        System.out.println();
-        printMatrix(CNE);
-        System.out.println();
-        printMatrix(CSW);
-        System.out.println();
-        printMatrix(CSE);
-        System.out.println();
         printMatrix(C);
-        System.out.println();
-        printMatrix(A);
-        System.out.println();
-        printMatrix(B);
     }
-
-
+    
     private static void printMatrix(int[][] matrix) {
         for(int r = 0; r < matrix.length; r++) {
             for(int c = 0; c < matrix[r].length; c++) {
